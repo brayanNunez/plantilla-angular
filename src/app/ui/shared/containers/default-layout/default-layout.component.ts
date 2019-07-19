4b36bd9ebe39180922c29539d7d43ba56a901ff5
brayanNunez <brayannr22@gmail.com>
2019-07-18 21:08:21 -0600

    layout

--- src/app/ui/shared/containers/default-layout/default-layout.component.ts
+++ src/app/ui/shared/containers/default-layout/default-layout.component.ts
@@ -1,27 +1,62 @@
-import { Component, OnDestroy } from '@angular/core';
-// import { navItems } from '@app/_nav';
-// import { navItems } from '@app/_nav';
- import { navItems } from '../../../../_nav';
-// import { faLock } from '@fortawesome/free-solid-svg-icons';
+// import { Component, OnDestroy } from '@angular/core';
+// // import { navItems } from '@app/_nav';
+// // import { navItems } from '@app/_nav';
+//  import { navItems } from '../../../../_nav';
+// // import { faLock } from '@fortawesome/free-solid-svg-icons';
+
+// @Component({
+//   selector: 'app-default-layout',
+//   templateUrl: './default-layout.component.html',
+//   styleUrls: ['./default-layout.component.scss']
+// })
+// export class DefaultLayoutComponent implements OnDestroy {
+//   // public faLock = faLock;
+//   public navItems = navItems;
+//   public sidebarMinimized = true;
+//   private changes: MutationObserver;
+//   public element: HTMLElement = document.body;
+//   constructor() {
+//     this.changes = new MutationObserver(mutations => {
+//       this.sidebarMinimized = document.body.classList.contains(
+//         'sidebar-minimized'
+//       );
+//     });
+
+//     this.changes.observe(<Element>this.element, {
+//       attributes: true,
+//       attributeFilter: ['class']
+//     });
+//   }
+
+//   ngOnDestroy(): void {
+//     this.changes.disconnect();
+//   }
+// }
+
+
+
+
+import { Component, OnDestroy, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+// import { navItems } from '../../_nav';
+import { navItems } from '../../../../_nav';
+
 
 @Component({
-  selector: 'app-default-layout',
-  templateUrl: './default-layout.component.html',
-  styleUrls: ['./default-layout.component.scss']
+  selector: 'app-dashboard',
+  templateUrl: './default-layout.component.html'
 })
 export class DefaultLayoutComponent implements OnDestroy {
-  // public faLock = faLock;
   public navItems = navItems;
   public sidebarMinimized = true;
   private changes: MutationObserver;
-  public element: HTMLElement = document.body;
-  constructor() {
-    this.changes = new MutationObserver(mutations => {
-      this.sidebarMinimized = document.body.classList.contains(
-        'sidebar-minimized'
-      );
-    });
+  public element: HTMLElement;
+  constructor(@Inject(DOCUMENT) _document?: any) {
 
+    this.changes = new MutationObserver((mutations) => {
+      this.sidebarMinimized = _document.body.classList.contains('sidebar-minimized');
+    });
+    this.element = _document.body;
     this.changes.observe(<Element>this.element, {
       attributes: true,
       attributeFilter: ['class']
